2318bc8a040d1d07f5178be03208d715f22ee378
sreehari.n.k18@gmail.com <sreehari.n.k18@gmail.com>
2025-01-27 12:17:18 +0530

    emailService Completed

---
+++ ParkingPlatform.Model/DTO/EmailDtosFolder/Message.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingPlatform.Model.DTO.EmailDtosFolder
+{
+    public class Message
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+        public Message(string to, string subject, string content)
+        {
+            To = to;
+            Subject = subject;
+            Content = content;
+        }
+    }
+}
